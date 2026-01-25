f2cf634fc05323a86231dfb75d8f151a669bde43
rohitprajapati25 <pr4901958@gmail.com>
2026-01-25 22:09:30 +0530

    Commit Now

--- frontend/admin/src/api/axios.js
+++ frontend/admin/src/api/axios.js
@@ -32,7 +32,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  //  baseURL = `import.meta.env.VITE_API_BASE_URL`,
+
+  // baseURL: "http://localhost:5000/api",
+  baseURL: 'https://g97.onrender.com/api',
   headers: {
     "Content-Type": "application/json",
   },
